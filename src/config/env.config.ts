e240cc843a37526bd46a9a82156c86a97ff840c2
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-03-08 15:45:55 +0300

    implement .env parser

---
+++ src/config/env.config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import z from 'zod';
+
+if (process.env.NODE_ENV !== 'PROD')
+    dotenv.config();
+
+// im using zod to parse dotenv variables
+const ZodEnvSchema =  z.object({
+    NODE_ENV: z.enum(['DEV', 'PROD']),
+    POSTGRES_HOST: z.string().default('localhost'),
+    POSTGRES_USER: z.string().default('postgres'),
+    POSTGRES_PASSWORD: z.string().default('postgres'),
+    POSTGRES_PORT: z.coerce.number().default(5432),
+    POSTGRES_DATABASE: z.string(),
+});
+
+const result = ZodEnvSchema.safeParse(process.env);
+
+if (!result.success) {
+    console.error('Invalid environment variables:');
+    console.error(result.error);
+    process.exit(1);
+}
+
+const envConfig = result.data;
+
+export default envConfig;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import envConfig from './config/env.config.js';
 import { SchedulerService } from './scheduler/scheduler.service.js';
 import { TestJob } from './test.job.js';
 
@@ -5,7 +6,8 @@ const scheduler = new SchedulerService();
 
 const testJob = new TestJob('Hello!');
 
-const testJob2 = new TestJob('second job');
+const testJob2 = new TestJob(envConfig.POSTGRES_HOST);
+
 
 scheduler.addJob('test1', testJob, 2000);
 scheduler.addJob('test2', testJob2, 5000);
